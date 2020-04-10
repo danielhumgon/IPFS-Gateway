15b39a4991da62137025793dced5b32b38f42c51
danielhumgon <danielhumgon@gmail.com>
2020-04-10 02:23:57 -0400

    changed port of server

--- gateway.js
+++ gateway.js
@@ -7,13 +7,18 @@ const getPeers = require('./peers')
 const INTERVAL = 20000 // Recconnet to peers interval
 
 
+const config = {
+    Addresses: {
+        Gateway: "/ip4/165.227.82.120/tcp/8080"
+    }
+}
 
 
 const initGateway = async () => {
     try {
         const peers = await getPeers()
 
-        await node.startNode()
+        await node.startNode(null,config)
 
         if (peers && peers.length > 0) {
             await stayConnected(peers)
